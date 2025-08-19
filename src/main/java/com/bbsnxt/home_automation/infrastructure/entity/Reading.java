9a272acb67dc89f485ec158e7706e1b5bb02679a
Felipe <felipebbarbosasantos@gmail.com>
2025-08-18 22:46:16 -0300

    feat: populating classe Reading

--- src/main/java/com/bbsnxt/home_automation/infrastructure/entity/Reading.java
+++ src/main/java/com/bbsnxt/home_automation/infrastructure/entity/Reading.java
@@ -1,4 +1,32 @@
 package com.bbsnxt.home_automation.infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "reading")
+@Entity
 public class Reading {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long readingId;
+
+    @Column(name = "value", nullable = false)
+    private Double value;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    // Um sensor pode ter várias leituras
+    @ManyToOne
+    @JoinColumn(name = "sensor_id", nullable = false)
+    private Sensor sensor;
+
 }
