b44612099cf46f1abcc3a57174dabce95b609f3f
Felipe <felipebbarbosasantos@gmail.com>
2025-08-18 22:30:57 -0300

    feat: repository SensorRepository

--- src/main/java/com/bbsnxt/home_automation/infrastructure/repository/SensorRepository.java
+++ src/main/java/com/bbsnxt/home_automation/infrastructure/repository/SensorRepository.java
@@ -3,5 +3,5 @@ package com.bbsnxt.home_automation.infrastructure.repository;
 import com.bbsnxt.home_automation.infrastructure.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class SensorRepository extends JpaRepository<Sensor, Long> {
+public interface SensorRepository extends JpaRepository<Sensor, Long> {
 }
