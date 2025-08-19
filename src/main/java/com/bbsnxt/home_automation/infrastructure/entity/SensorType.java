3d8c4784c7a422a09830433f306b4fd9e74fce3e
Felipe <felipebbarbosasantos@gmail.com>
2025-08-18 22:44:33 -0300

    feat: populating classe SensorType

--- src/main/java/com/bbsnxt/home_automation/infrastructure/entity/SensorType.java
+++ src/main/java/com/bbsnxt/home_automation/infrastructure/entity/SensorType.java
@@ -1,4 +1,31 @@
 package com.bbsnxt.home_automation.infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "sensor_type")
+@Entity
 public class SensorType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long sensorTypeId;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    // Um tipo de sensor pode ter vários sensores
+    @OneToMany(mappedBy = "sensorType")
+    private List<Sensor> sensors;
+
 }
